0752101af27c0a5ed0693ffcc401cb1cf7f6960d
Nikhil Kholiya <nikhilkholiya08@gmail.com>
2026-01-21 15:53:24 +0530

    fix import error

--- src/components/ProtectedRoute.tsx
+++ src/components/ProtectedRoute.tsx
@@ -10,7 +10,7 @@ const ProtectedRoute: React.FC<ProtectedRouteProps> = ({ children }) => {
   const { isAuthenticated } = useAuth();
 
   if (!isAuthenticated) {
-    return <Navigat to="/login" replace />;
+    return <Navigate to="/login" replace />;
   }
 
   return <>{children}</>;
